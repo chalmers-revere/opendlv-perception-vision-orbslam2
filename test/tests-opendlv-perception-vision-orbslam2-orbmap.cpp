696023b5a218e2d2549d37fc3c2dfc94e1f7e1e4
Pontus Pohl <pontus.pohl@endian.se>
2018-03-20 17:28:50 +0100

    fix macros in test case

--- test/tests-opendlv-perception-vision-orbslam2-orbmap.cpp
+++ test/tests-opendlv-perception-vision-orbslam2-orbmap.cpp
@@ -38,7 +38,7 @@ TEST_CASE("testAddOrbKeyFrame")
     opendlv::logic::sensation::OrbFrame *frame = new opendlv::logic::sensation::OrbFrame(left, right, keyPoints, tcw);
     frame->Id = 10;
     std::shared_ptr<opendlv::logic::sensation::OrbFrame> keyFrame(frame);
-    TS_ASSERT_THROWS_NOTHING(orbMap.PushOrbKeyFrame(keyFrame));
+    REQUIRE_NOTHROW(orbMap.PushOrbKeyFrame(keyFrame));
 }
 TEST_CASE("testMaxKeyFrameId")
 {
@@ -51,25 +51,25 @@ TEST_CASE("testMaxKeyFrameId")
     opendlv::logic::sensation::OrbFrame *frame = new opendlv::logic::sensation::OrbFrame(left, right, keyPoints, tcw);
     frame->Id = 10;
     std::shared_ptr<opendlv::logic::sensation::OrbFrame> keyFrame(frame);
-    TS_ASSERT_THROWS_NOTHING(orbMap.PushOrbKeyFrame(keyFrame));
-    TS_ASSERT_EQUALS(orbMap.MaxKeyFrameId(), 10);
+    REQUIRE_NOTHROW(orbMap.PushOrbKeyFrame(keyFrame));
+    REQUIRE(orbMap.MaxKeyFrameId() ==  10);
 }
 TEST_CASE("testIncrementMajorChangeIndex")
 {
     opendlv::logic::sensation::OrbMap orbMap;
-    TS_ASSERT_THROWS_NOTHING(orbMap.IncrementMajorChangeIndex());
+    REQUIRE_NOTHROW(orbMap.IncrementMajorChangeIndex());
 }
 TEST_CASE("testLastMajorChangeIndex")
 {
     opendlv::logic::sensation::OrbMap orbMap;
-    TS_ASSERT_THROWS_NOTHING(orbMap.IncrementMajorChangeIndex());
-    TS_ASSERT_EQUALS(orbMap.LastMajorChangeIndex(), 1);
+    REQUIRE_NOTHROW(orbMap.IncrementMajorChangeIndex());
+    REQUIRE(orbMap.LastMajorChangeIndex() ==  1);
 }
 TEST_CASE("testAddOrbMapPoint")
 {
     opendlv::logic::sensation::OrbMap orbMap;
     std::shared_ptr<opendlv::logic::sensation::OrbMapPoint> mapPoint;
-    TS_ASSERT_THROWS_NOTHING(orbMap.PushOrbMapPoint(mapPoint));
+    REQUIRE_NOTHROW(orbMap.PushOrbMapPoint(mapPoint));
 }
 TEST_CASE("testSetReferenceMapPoints")
 {
@@ -77,7 +77,7 @@ TEST_CASE("testSetReferenceMapPoints")
     std::shared_ptr<opendlv::logic::sensation::OrbMapPoint> mapPoint;
     std::vector<std::shared_ptr<opendlv::logic::sensation::OrbMapPoint>> referenceMapPoints;
     referenceMapPoints.push_back(mapPoint);
-    TS_ASSERT_THROWS_NOTHING(orbMap.SetReferenceMapPoints(referenceMapPoints));
+    REQUIRE_NOTHROW(orbMap.SetReferenceMapPoints(referenceMapPoints));
 }
 TEST_CASE("testOrbKeyFramesCount")
 {
@@ -90,15 +90,15 @@ TEST_CASE("testOrbKeyFramesCount")
     opendlv::logic::sensation::OrbFrame *frame = new opendlv::logic::sensation::OrbFrame(left, right, keyPoints, tcw);
     frame->Id = 10;
     std::shared_ptr<opendlv::logic::sensation::OrbFrame> keyFrame(frame);
-    TS_ASSERT_THROWS_NOTHING(orbMap.PushOrbKeyFrame(keyFrame));
-    TS_ASSERT_EQUALS(orbMap.OrbKeyFramesCount(), 1);
+    REQUIRE_NOTHROW(orbMap.PushOrbKeyFrame(keyFrame));
+    REQUIRE(orbMap.OrbKeyFramesCount() ==  1);
 }
 TEST_CASE("testOrbMapPointsCount")
 {
     opendlv::logic::sensation::OrbMap orbMap;
     std::shared_ptr<opendlv::logic::sensation::OrbMapPoint> mapPoint;
-    TS_ASSERT_THROWS_NOTHING(orbMap.PushOrbMapPoint(mapPoint));
-    TS_ASSERT_EQUALS(orbMap.OrbMapPointsCount(), 1);
+    REQUIRE_NOTHROW(orbMap.PushOrbMapPoint(mapPoint));
+    REQUIRE(orbMap.OrbMapPointsCount() ==  1);
 }
 TEST_CASE("testDeleteOrbKeyFrame")
 {
@@ -111,26 +111,26 @@ TEST_CASE("testDeleteOrbKeyFrame")
     opendlv::logic::sensation::OrbFrame *frame = new opendlv::logic::sensation::OrbFrame(left, right, keyPoints, tcw);
     frame->Id = 10;
     std::shared_ptr<opendlv::logic::sensation::OrbFrame> keyFrame(frame);
-    TS_ASSERT_THROWS_NOTHING(orbMap.PushOrbKeyFrame(keyFrame));
-    TS_ASSERT_EQUALS(orbMap.OrbKeyFramesCount(), 1);
-    TS_ASSERT_THROWS_NOTHING(orbMap.DeleteOrbKeyFrame(keyFrame));
-    TS_ASSERT_EQUALS(orbMap.OrbKeyFramesCount(), 0);
+    REQUIRE_NOTHROW(orbMap.PushOrbKeyFrame(keyFrame));
+    REQUIRE(orbMap.OrbKeyFramesCount() ==  1);
+    REQUIRE_NOTHROW(orbMap.DeleteOrbKeyFrame(keyFrame));
+    REQUIRE(orbMap.OrbKeyFramesCount() ==  0);
 }
 TEST_CASE("testDeleteOrbMapPoint")
 {
     opendlv::logic::sensation::OrbMap orbMap;
     std::shared_ptr<opendlv::logic::sensation::OrbMapPoint> mapPoint;
-    TS_ASSERT_THROWS_NOTHING(orbMap.PushOrbMapPoint(mapPoint));
-    TS_ASSERT_EQUALS(orbMap.OrbMapPointsCount(), 1);
-    TS_ASSERT_THROWS_NOTHING(orbMap.DeleteOrbMapPoint(mapPoint));
-    TS_ASSERT_EQUALS(orbMap.OrbMapPointsCount(), 0);
+    REQUIRE_NOTHROW(orbMap.PushOrbMapPoint(mapPoint));
+    REQUIRE(orbMap.OrbMapPointsCount() ==  1);
+    REQUIRE_NOTHROW(orbMap.DeleteOrbMapPoint(mapPoint));
+    REQUIRE(orbMap.OrbMapPointsCount() ==  0);
 }
 TEST_CASE("testReset")
 {
     opendlv::logic::sensation::OrbMap orbMap;
     std::shared_ptr<opendlv::logic::sensation::OrbMapPoint> mapPoint;
-    TS_ASSERT_THROWS_NOTHING(orbMap.PushOrbMapPoint(mapPoint));
-    TS_ASSERT_EQUALS(orbMap.OrbMapPointsCount(), 1);
+    REQUIRE_NOTHROW(orbMap.PushOrbMapPoint(mapPoint));
+    REQUIRE(orbMap.OrbMapPointsCount() ==  1);
 
     cv::Mat left, right, tcw;
     left = cv::Mat::zeros(1000, 1000, CV_32F);
@@ -140,14 +140,14 @@ TEST_CASE("testReset")
     opendlv::logic::sensation::OrbFrame *frame = new opendlv::logic::sensation::OrbFrame(left, right, keyPoints, tcw);
     frame->Id = 10;
     std::shared_ptr<opendlv::logic::sensation::OrbFrame> keyFrame(frame);
-    TS_ASSERT_THROWS_NOTHING(orbMap.PushOrbKeyFrame(keyFrame));
-    TS_ASSERT_EQUALS(orbMap.OrbKeyFramesCount(), 1);
+    REQUIRE_NOTHROW(orbMap.PushOrbKeyFrame(keyFrame));
+    REQUIRE(orbMap.OrbKeyFramesCount() ==  1);
 
     std::vector<std::shared_ptr<opendlv::logic::sensation::OrbMapPoint>> referenceMapPoints;
     referenceMapPoints.push_back(mapPoint);
-    TS_ASSERT_THROWS_NOTHING(orbMap.SetReferenceMapPoints(referenceMapPoints));
+    REQUIRE_NOTHROW(orbMap.SetReferenceMapPoints(referenceMapPoints));
 
     orbMap.Reset();
-    TS_ASSERT_EQUALS(orbMap.OrbMapPointsCount(), 0);
-    TS_ASSERT_EQUALS(orbMap.OrbKeyFramesCount(), 0);
+    REQUIRE(orbMap.OrbMapPointsCount() ==  0);
+    REQUIRE(orbMap.OrbKeyFramesCount() ==  0);
 }
